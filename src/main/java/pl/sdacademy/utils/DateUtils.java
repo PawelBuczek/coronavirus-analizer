ac3e4b158fc477a97d781d3fc49a592ce9a5e92e
Hincak <hincak@gmail.com>
2020-09-23 20:31:41 +0200

    D2 update, D4 Dodaj do klasy DateUtils metodę, która przyjmie parametr - listę dat, a która zwróci liczbę dni, między dwoma najodleglejszymi datami.

--- src/main/java/pl/sdacademy/utils/DateUtils.java
+++ src/main/java/pl/sdacademy/utils/DateUtils.java
@@ -2,10 +2,19 @@ package pl.sdacademy.utils;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+import java.util.List;
 
 public class DateUtils {
 
-    public static long daysBetween (LocalDate startDate, LocalDate endDate) {
+    //Zadanie D2
+    public long daysBetween (LocalDate startDate, LocalDate endDate) {
         return startDate.until(endDate, ChronoUnit.DAYS);
+    }
+    //Zadanie D4
+    public long daysBetweenFromList(List<LocalDate> dateList){
+        LocalDate minDate = Collections.min(dateList);
+        LocalDate maxDate = Collections.max(dateList);
+        return minDate.until(maxDate,ChronoUnit.DAYS);
     }
 }
