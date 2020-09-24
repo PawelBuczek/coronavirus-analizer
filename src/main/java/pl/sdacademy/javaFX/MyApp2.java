4eec62511d2d73d827eab7ace7c39b94b5feb875
PawelBuczek <51830329+PawelBuczek@users.noreply.github.com>
2020-09-24 18:15:50 +0200

    FX3 Znajdź sposób, jak klikając przycisk JavyFX otworzyć nowe okno. Utwórz przykład i umieść w projekcie.

--- src/main/java/pl/sdacademy/javaFX/MyApp2.java
+++ src/main/java/pl/sdacademy/javaFX/MyApp2.java
@@ -1,8 +1,10 @@
 package pl.sdacademy.javaFX;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -10,9 +12,19 @@ public class MyApp2 extends Application {
     @Override
     public void start(Stage stage) {
         VBox firstMainVBox = new VBox();
-        Button button = new Button("Start Game");
+        Button button = new Button("ShowGraph");
+        button.setOnAction(event -> showGraph(stage));
+        firstMainVBox.getChildren().add(button);
+        firstMainVBox.setAlignment(Pos.CENTER);
         Scene scene = new Scene(firstMainVBox, 400, 600);
         stage.setScene(scene);
         stage.show();
+    }
+    private void showGraph(Stage stage) {
+        VBox graphVBox = new VBox();
+        Label topInfo = new Label("Here be graph");
+        graphVBox.getChildren().add(topInfo);
+        Scene graphScene = new Scene(graphVBox, 600, 800);
+        stage.setScene(graphScene);
     }
 }
