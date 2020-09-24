c030f788f327fb47a3d7d3c63450d75bfa611d66
Sharin6 <agaberg@op.pl>
2020-09-24 20:10:19 +0200

    Utwórz interfejs użytkownika - klasę MainUI, rozszerzającą klasę VBox, zawierającą: Etykiety tekstowe: - Menu główne - Dane zaktualizowano XX.XX.XXXX Przyciski: - Aktualizuj dane - Wyświetl wykres

--- src/main/java/pl/sdacademy/javaFX/MainUI.java
+++ src/main/java/pl/sdacademy/javaFX/MainUI.java
@@ -1,33 +1,16 @@
 package pl.sdacademy.javaFX;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
-
-public class MainUI extends Application {
-    private Label label;
-
-    @Override
-    public void start(Stage stage) {
-        VBox vBox = new VBox();
-        vBox.getChildren().add(new Label("Menu główne"));
-        vBox.getChildren().add(new Label("Dane zaktualizowano XX.XX.XXXX"));
-
-        vBox.getChildren().add(new Button("Aktualizuj dane"));
-        vBox.getChildren().add(new Button("Wyświetl wykres"));
-
-        Scene scene = new Scene(vBox, 600, 600);
-        stage.setScene(scene);
-        stage.show();
+public class MainUI extends VBox {
+    public MainUI() {
+        super();
+        this.getChildren().add(new Label("Menu główne"));
+        this.getChildren().add(new Label("Dane zaktualizowano XX.XX.XXXX"));
+        this.getChildren().add(new Button("Aktualizuj dane"));
+        this.getChildren().add(new Button("Wyświetl wykres"));
+        
     }
-
-    // run test
-    public static void main(String[] args) {
-        launch();
-    }
-
 }
