161a5e24ca7586fa79694cec214c6cd2df928928
pawel.buczek@email.com <Ksiazka@1>
2020-09-28 19:49:11 +0200

    DC2 Utwórz klasę ApiObjectToEntityMapper, która będzie mapperem ("przerabiaczem) obiektów typów zdefiniowanych w DA3 na obiekty reprezentujące kraje (DB2). Oferuje jedną metodę - map, realizującą tę funkcjonalność.

--- src/main/java/pl/sdacademy/coronavirus/Main.java
+++ src/main/java/pl/sdacademy/coronavirus/Main.java
@@ -21,54 +21,14 @@ public class Main {
             e.printStackTrace();
         }
 
+        System.out.println("second quick test:" + System.lineSeparator());
+        ApiObjectToEntityMapper mapper = new ApiObjectToEntityMapper();
         try {
             List<CovidDataForDateAndCountryFromAPI> listDataFromAPI = ApiDataProvider.getListOfCovidCountryStatusFromJason();
-            listDataFromAPI.sort(Comparator.comparing(CovidDataForDateAndCountryFromAPI::getDate));
-            listDataFromAPI.sort(Comparator.comparing(CovidDataForDateAndCountryFromAPI::getCountryName));
-            // example of getting the data out of the list
-            System.out.println(listDataFromAPI.size());
-            System.out.println(listDataFromAPI.get(3));
-
-            // since we don't have all numbers in our dataset, I am calculating them here
-            // I am also adding the Country (as Country object rather than String)
-            System.out.println("Here be dragons:" + System.lineSeparator());
-            Set<Country> countries = new HashSet<>();
-            listDataFromAPI.forEach(covidRow -> countries.add(new Country(covidRow.getCountryName())));
-            List<DateCountryCovidStatus> covidUpdates = new ArrayList<>();
-            listDataFromAPI.forEach(
-                    covidRow -> {
-                        if (!countryBefore.equals(covidRow.getCountryName())) {
-                            deathsBefore = 0L;
-                            casesBefore = 0L;
-                            recoveredBefore = 0L;
-                            countryBefore = covidRow.getCountryName();
-                        }
-                        covidUpdates.add(
-                                new DateCountryCovidStatus(
-                                        countries.stream().filter(country -> country.getTwoLetterCode().equals(covidRow.getCountryName()))
-                                                .findAny().orElse(null),
-                                        covidRow.getDate(),
-                                        covidRow.getTotalCases(),
-                                        covidRow.getTotalDeaths(),
-                                        // because that data we get from API is clearly not correct with those,
-                                        // the below is making sure that the TotalRecovered number decreasing
-                                        Math.max(recoveredBefore, covidRow.getTotalRecovered()),
-                                        covidRow.getTotalCases() - casesBefore,
-                                        covidRow.getTotalDeaths() - deathsBefore,
-                                        covidRow.getTotalRecovered() - recoveredBefore
-                                ));
-                        casesBefore = covidRow.getTotalCases();
-                        deathsBefore = covidRow.getTotalDeaths();
-                        recoveredBefore = Math.max(recoveredBefore, covidRow.getTotalRecovered());
-                    });
-            //just to view some of the interesting days in order from US for example
-            List<DateCountryCovidStatus> covidUpdatesUS = covidUpdates.stream()
-                    .filter(covidUpdate -> covidUpdate.getCountry().getTwoLetterCode().equals("US"))
-                    .collect(Collectors.toList());
-            for (int i = 40; i < 65; i++) {
-                System.out.println(covidUpdatesUS.get(i));
+            List<DateCountryCovidStatus> mappedList = mapper.map(listDataFromAPI);
+            for (int i = 1928; i < 1948; i++) {
+                System.out.println(mappedList.get(i));
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
