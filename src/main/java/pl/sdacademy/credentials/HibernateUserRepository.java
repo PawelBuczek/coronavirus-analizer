d4dd709e9b0cccf749ed1326f673ae85b78a1a33
Marta Kuśpit <mkuspit@gmail.com>
2020-09-28 19:14:03 +0200

    metoda readID i poprawienie readAll

--- src/main/java/pl/sdacademy/credentials/HibernateUserRepository.java
+++ src/main/java/pl/sdacademy/credentials/HibernateUserRepository.java
@@ -1,6 +1,8 @@
 package pl.sdacademy.credentials;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,16 +11,28 @@ public class HibernateUserRepository implements UserRepository {
     //Utwórz implementację interfejsu UserRepository - HibernateUserRepository - implementacja posiada pole - fabrykę sesji.
     // Implementacje metod interfejsu UserRepository powinny być najprostsze możliwe (nic nie robią i zwracają null, jeśli mają zwrócić wartość).
     private SessionFactory sessionFactory;
-    private List<User> users = new ArrayList<>();
+
+
+    public HibernateUserRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     @Override
     public User readById(int id) {
-        return null;
+        Session session = sessionFactory.openSession();
+        User user= session.get(User.class,id);
+        session.close();
+        return user;
     }
 
     @Override
     public List<User> readAll() {
-        return users;
+        Session session = sessionFactory.openSession();
+        Query<User> query1 = session.createQuery("SELECT id,firstName," +
+                "lastName,dateOfBirth,admin FROM User", User.class);
+        List<User> result = query1.getResultList();
+        return result;
+
 
     }
 
