08ec21d778932445aac98a5131aeb58178750dd2
pawel.buczek@email.com <Ksiazka@1>
2020-09-28 19:21:08 +0200

    added inputs to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,6 @@ module pl.sdacademy.javaFx {
     requires java.persistence;
     requires org.hibernate.orm.core;
     exports pl.sdacademy.javaFX;
+    exports pl.sdacademy.coronavirus;
+    requires java.naming;
 }
