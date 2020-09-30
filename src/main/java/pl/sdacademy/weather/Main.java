5a3a5fca7318cff5ac834596d2528a9222c1614f
Marta Kuśpit <mkuspit@gmail.com>
2020-09-30 18:29:43 +0200

    zadanka od D1 do D4

--- src/main/java/pl/sdacademy/weather/Main.java
+++ src/main/java/pl/sdacademy/weather/Main.java
@@ -1,9 +1,47 @@
 package pl.sdacademy.weather;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import pl.sdacademy.coronavirus.SessionFactoryProvider;
+import pl.sdacademy.credentials.User;
+
+import java.util.List;
+
 public class Main {
+    public static SessionFactory sessionFactory;
+
+
     public static void main(String[] args) {
-        Weather weather = new Weather(12.48,56.68,6.22);
-        System.out.println(weather.getCloudCover());
-        System.out.println(weather.getId());
+
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+        readData(sessionFactory);
+
+
+
+
+
+
+        sessionFactory.close();
+
     }
+
+
+    public static void readData(SessionFactory sessionFactory) {
+
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query<Weather> query = session.createQuery("FROM Weather ", Weather.class);
+        List<Weather> result = query.getResultList();
+        System.out.println(result);
+        transaction.commit();
+        session.close();
+    }
+
+
+
 }
